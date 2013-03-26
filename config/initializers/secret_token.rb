4a7b1a7b29f46f988826c91bc6f172d920ecb026
Vincent Wilson <vincent.movieman@gmail.com>
2013-03-26 18:14:11 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRealAdventureTime::Application.config.secret_token = '3e4fbedd51a2c0207280ac86b7ae9279777da6ae4a4c1086ab72635f712c8ff321e4f9c78050af39d97161ea96425f85f98ddd438cab21d46a892323b025a35b'
