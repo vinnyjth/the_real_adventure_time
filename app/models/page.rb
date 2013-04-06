737c557bff5018fcd0fa6e5c37cae9bcf0554d16
Vincent Wilson <vincent.movieman@gmail.com>
2013-04-06 16:50:42 -0400

    Relations work

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,10 +4,11 @@ class Page < ActiveRecord::Base
 
 
 
-  has_many :paths, :foreign_key => :page_to_id, :dependent => :destroy
-  has_many :reverse_paths , :class_name => :Path, :foreign_key => :page_from_id 
+  has_many :reverse_paths, :class_name => :Path, :foreign_key => :page_to_id, :dependent => :destroy
+  has_many :paths ,  :foreign_key => :page_from_id, :dependent => :destroy
   
-  has_many :pages, :through => :paths, :source => :page_from, :dependent => :destroy
+  has_many :pages, :through => :reverse_paths, :source => :page_from
+  has_many :reverse_pages, :through => :paths, :source => :page_to
 
-  accepts_nested_attributes_for  :paths, :reverse_paths, :pages
+  accepts_nested_attributes_for  :paths, :reverse_paths, :pages, :reverse_pages
 end
