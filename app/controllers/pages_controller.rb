4aa4e627cd5db09d9e9dfdf178838edc7cf794eb
Vincent Wilson <vincent.movieman@gmail.com>
2013-04-13 21:35:57 -0400

    made text pretty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -147,9 +147,11 @@ class PagesController < ApplicationController
     def check_branch_access
       if params.has_key?(:root_id)
         @root = Page.find(params[:root_id])
-        unless @root.group.users.all.include? current_user
+        if @root.editable == false
+        unless @root.group.users.all.include? current_user || 
           flash[:error] = "You don't own the node page. Try creating your own node!"
           redirect_to @root
+        end
         end
       end
     end
